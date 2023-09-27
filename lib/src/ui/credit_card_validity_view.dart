9b5733db9d00d5e9fa8dfe980b7f36be73b462fa
Utpal Barman <utpal.barman.bd@gmail.com>
2023-09-28 01:53:52 +0600

    feat: Added showValidFrom and showValidThru parameters for visibility control.

--- lib/src/ui/credit_card_validity_view.dart
+++ lib/src/ui/credit_card_validity_view.dart
@@ -8,6 +8,8 @@ class CreditCardValidityView extends StatelessWidget {
     super.key,
     required this.validFromMasked,
     required this.validThruMasked,
+    this.showValidFrom = true,
+    this.showValidThru = true,
   });
 
   ///
@@ -16,47 +18,75 @@ class CreditCardValidityView extends StatelessWidget {
   ///
   final String validThruMasked;
 
+  /// Determines whether to show the "Valid From" segment on the card.
+  ///
+  /// If set to `true`, the "Valid From" segment will be displayed.
+  /// If set to `false`, it will be hidden.
+  /// The default value is `true`.
+  final bool showValidFrom;
+
+  /// Determines whether to show the "Valid Thru" segment on the card.
+  ///
+  /// If set to `true`, the "Valid Thru" segment will be displayed.
+  /// If set to `false`, it will be hidden.
+  /// The default value is `true`.
+  final bool showValidThru;
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        const SizedBox(
-          width: 24,
-          child: Text(
-            'VALID FROM',
-            style: TextStyle(
-              color: Color.fromARGB(255, 200, 200, 200),
-              fontSize: 5,
-              height: 1.2,
-            ),
-            textAlign: TextAlign.center,
+        Visibility.maintain(
+          visible: showValidFrom,
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 24,
+                child: Text(
+                  'VALID FROM',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 200, 200, 200),
+                    fontSize: 5,
+                    height: 1.2,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              if (validFromMasked == null)
+                const SizedBox(width: 38)
+              else
+                CreditCardText(
+                  validFromMasked!,
+                  letterSpacing: 2,
+                  fontSize: 9,
+                ),
+            ],
           ),
         ),
-        if (validFromMasked == null)
-          const SizedBox(width: 38)
-        else
-          CreditCardText(
-            validFromMasked!,
-            letterSpacing: 2,
-            fontSize: 9,
-          ),
         const SizedBox(width: 24),
-        const SizedBox(
-          width: 24,
-          child: Text(
-            'VALID THRU',
-            style: TextStyle(
-              color: Color.fromARGB(255, 200, 200, 200),
-              fontSize: 5,
-              height: 1.2,
-            ),
-            textAlign: TextAlign.center,
+        Visibility.maintain(
+          visible: showValidThru,
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 24,
+                child: Text(
+                  'VALID THRU',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 200, 200, 200),
+                    fontSize: 5,
+                    height: 1.2,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              CreditCardText(
+                validThruMasked,
+                letterSpacing: 2,
+                fontSize: 9,
+              ),
+            ],
           ),
-        ),
-        CreditCardText(
-          validThruMasked,
-          letterSpacing: 2,
-          fontSize: 9,
         ),
       ],
     );
